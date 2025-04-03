57bd3bbc6ead75198752c26693f530759cf4a77a
akar@M3Air <aks_002@outlook.com>
2025-04-03 07:37:44 -0700

    config - keep default base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/corolla-hybrid-inventory-browser',
   images: {
     unoptimized: true,
   },
