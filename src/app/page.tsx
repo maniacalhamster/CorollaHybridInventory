81664e7537d7c7f88faa321c94097c94f8f5925f
akar@M3Air <aks_002@outlook.com>
2025-04-03 18:38:34 -0700

    lint - resolve csr bailout issue by wrapping main table in Suspence boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { InventoryTable } from "@/components/InventoryTable"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <div className="max-w-[80%] mx-auto py-10">
       <h1 className="text-2xl font-bold mb-4">Inventory Table</h1>
-      <InventoryTable />
+      <Suspense>
+        <InventoryTable />
+      </Suspense>
     </div>
   )
 }
